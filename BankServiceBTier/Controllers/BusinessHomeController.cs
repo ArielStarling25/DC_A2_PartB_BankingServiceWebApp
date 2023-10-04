7599e7eaee0245cceb4dee83a5bb8a3317271475
Ariel Starling <arielstarling25@gmail.com>
2023-10-04 15:36:19 +0800

    BusinessTier_Work 1

--- BankServiceBTier/Controllers/BusinessHomeController.cs
+++ BankServiceBTier/Controllers/BusinessHomeController.cs
@@ -4,11 +4,11 @@ using System.Diagnostics;
 
 namespace BankServiceBTier.Controllers
 {
-    public class HomeController : Controller
+    public class BusinessHomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<BusinessHomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public BusinessHomeController(ILogger<BusinessHomeController> logger)
         {
             _logger = logger;
         }
---
+++ BankServiceBTier/Controllers/BusinessTierController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using Newtonsoft.Json;
+using BankDataWebService.Models;
+
+namespace BankServiceBTier.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusinessTierController : ControllerBase
+    {
+    }
+}
