cfc44fbf235a4db41bf13848572ff6bfe0578688
Ariel Starling <arielstarling25@gmail.com>
2023-10-05 20:01:46 +0800

    Basic Login Screen works :))

--- BankServiceGUI/Controllers/HomeController.cs
+++ BankServiceGUI/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using BankDataWebService.Models;
 using Microsoft.Identity.Client;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace BankServiceGUI.Controllers
 {
@@ -21,10 +23,70 @@ namespace BankServiceGUI.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Index(int sessionId)
+        {
+            if(sessionId == 12345)
+            {
+                ViewBag.Message = "LoggedIn";
+            }
+            return View();
+        }
+
         [HttpPost]
         public async Task<IActionResult> Index(string email, string password)
         {
-            
+            List<Profile> profiles = null;
+            RestClient client = new RestClient(httpURL);
+            RestRequest req = new RestRequest("/api/bprofile", Method.Get);
+            RestResponse response = await client.GetAsync(req);
+            profiles = JsonConvert.DeserializeObject<List<Profile>>(response.Content);
+            if (profiles == null)
+            {
+                ViewBag.Error = "Error in obtaining profile information";
+            }
+            else
+            {
+                Profile profile = new Profile();
+                if(profileExists(email, profiles, out profile))
+                {
+                    if (password.Equals(profile.password))
+                    {
+                        ViewBag.Message = "LoggedIn";
+                        ViewBag.ProfileName = profile.name;
+                        ViewBag.ProfileEmail = profile.email;
+                        ViewBag.ProfileAddr = profile.address;
+                        ViewBag.ProfilePhone = profile.phone;
+                        ViewBag.ProfilePassword = profile.password;
+                        ViewBag.ProfilePictureData = profile.picture;
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Password is incorrect!";
+                    }
+                }
+                else
+                {
+                    ViewBag.Error = "Email does not exist!";
+                }
+            }
+            return View();
+        }
+
+        private bool profileExists(string email, List<Profile> profiles, out Profile profile)
+        {
+            bool found = false;
+            profile = null;
+            foreach(Profile prof in profiles)
+            {
+                if (email.Equals(prof.email))
+                {
+                    found = true;
+                    profile = prof;
+                    break;
+                }
+            }
+            return found;
         }
 
         [HttpGet]
