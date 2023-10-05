529428da8906fdd1e047164a785ef02601b73b2b
Ariel Starling <arielstarling25@gmail.com>
2023-10-05 18:48:47 +0800

    Working on HomeController

--- BankServiceGUI/Controllers/HomeController.cs
+++ BankServiceGUI/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 ﻿using BankServiceGUI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using BankDataWebService.Models;
+using Microsoft.Identity.Client;
 
 namespace BankServiceGUI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly string httpURL = "http://localhost:5103";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -18,6 +21,22 @@ namespace BankServiceGUI.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Index(string email, string password)
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Profile(int sessionId)
+        {
+            if(sessionId == 12345)
+            {
+                ViewBag.Message = "LoggedIn";
+            }
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
@@ -26,7 +45,7 @@ namespace BankServiceGUI.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new Models.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
